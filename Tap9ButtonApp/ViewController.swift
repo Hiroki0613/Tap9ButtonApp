e793128ce20586674d2c40b900dda60afa3ba73e
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-15 21:57:41 +0900

    Initial Commit

---
+++ Tap9ButtonApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tap9ButtonApp
+//
+//  Created by 近藤宏輝 on 2020/01/15.
+//  Copyright © 2020 宏輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
