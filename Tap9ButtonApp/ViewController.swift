a63005c8e9b60acd3c35d030a3723a9e07af3538
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-15 22:01:46 +0900

    上部３つのボタンを実装

--- Tap9ButtonApp/ViewController.swift
+++ Tap9ButtonApp/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+     var imageButton:UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for i in 0...8{
+             imageButton = UIButton()
+             
+             switch i {
+             case 0,1,2:
+                 imageButton.tag = i
+                 imageButton.frame = CGRect(x: CGFloat(i) * self.view.frame.size.width/3, y: self.view.frame.size.width/3 * 1, width: self.view.frame.width/3, height: self.view.frame.size.width/3)
+                 imageButton.setTitle("\(i)", for: .normal)
+                 imageButton.addTarget(self, action: #selector(tapButton(_ :)), for: .touchUpInside)
+                 break
+            }
     }
 
 
